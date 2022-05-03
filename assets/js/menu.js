ef916ad6adbc8ef56a3f723f0c43198b5c330f32
laihyeong <102306323+laihyeong@users.noreply.github.com>
2022-05-03 23:12:49 +0900

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector("#menu");
+const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".logo-readium");
 
 const onMenuClicked = () => {
