2a56abab92fca895a9b6ee0dba912b0d9988f6d9
laihyeong <102306323+laihyeong@users.noreply.github.com>
2022-05-03 23:15:51 +0900

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".logo-readium");
 
 const onMenuClicked = () => {
-  menu.style.left = "0px";
+  menu.classList.toggle('menu-open');
 }
 
 menuBtn.addEventListener("click", onMenuClicked);
