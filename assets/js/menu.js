f1db74533551ba7a2b71f77448424a21bda6e254
laihyeong <102306323+laihyeong@users.noreply.github.com>
2022-05-03 23:28:40 +0900

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,11 @@
 const nav = document.querySelector("nav");
-const menu = documnet.querySelector(".menu");
-const menuBtn = document.querySelector(".logo-readium");
+const menu = documnet.getElementsByClassName("menu");
+const menuBtn = document.getElementsByClassName("logo-readium");
 
 const onMenuClicked = () => {
   nav.classList.toggle('nav-open');
   menu.classList.toggle('menu-open');
+  console.log("working");
 }
 
 menuBtn.addEventListener("click", onMenuClicked);
